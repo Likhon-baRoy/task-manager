130393ad995d1fe66855ac367e88b0801b30e22d
Likhon Baroi <likhonhere007@proton.me>
2026-04-19 23:15:47 +0600

    used data-table to get easy search, pagination, filter etc.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>Task Manager</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
@@ -98,8 +99,20 @@
 
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
+        // Initialize DataTable
+        $('#taskTable').DataTable({
+            pageLength: 10,
+            ordering: true,
+            responsive: true,
+            language: {
+                search: "Search tasks:",
+                lengthMenu: "Show _MENU_ tasks"
+            }
+        });
+
         // Description Modal
         $('#descModal').on('show.bs.modal', function(event) {
             const button = $(event.relatedTarget);
